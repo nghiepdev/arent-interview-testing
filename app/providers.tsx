4055bd884a6957fef7e8527e8d59e9eb70b3387c
Nghiep <me@nghiep.dev>
2023-09-11 14:19:51 +0700

    style(tailwind): theme configuration

---
+++ app/providers.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+export default function Provider({children}: React.PropsWithChildren) {
+  return <>{children}</>;
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,49 @@
-import type { Config } from 'tailwindcss'
+import type {Config} from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 const config: Config = {
+  darkMode: 'class',
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '960px',
+    },
+    colors: {
+      light: '#FFFFFF',
+      'primary-300': '#FFCC21',
+      'primary-400': '#FF963C',
+      'primary-500': '#EA6C00',
+      'secondary-300': '#8FE9D0',
+      'dark-600': '#2E2E2E',
+      'dark-500': '#414141',
+      'gray-400': '#777777',
+    },
     extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)', ...defaultTheme.fontFamily.sans],
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'primary-gradient':
+          'linear-gradient(33deg, #FFCC21 8.75%, #FF963C 86.64%)',
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('tailwind-scrollbar-hide'),
+  ],
+};
+
+export default config;
